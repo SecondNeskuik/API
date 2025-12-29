d5fc6e769be39b72632320a49b5abfe216d58237
neskuik_k <giraudeau.malone@icloud.com>
2025-12-29 22:29:29 +0100

    fix: api.

--- src/main/java/io/neskdev/api/NeskAPI.java
+++ src/main/java/io/neskdev/api/NeskAPI.java
@@ -4,13 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 import io.neskdev.api.database.DatabaseManager;
 import io.neskdev.api.rank.RankRepository;
-import io.neskdev.api.user.UserFactory;
-import io.neskdev.api.user.UserRepository;
 
 public interface NeskAPI {
 
-    UserFactory getUserFactory();
-    UserRepository getUserRepository();
     RankRepository getRankRepository();
 
     DatabaseManager getDatabaseManager();
--- src/main/java/io/neskdev/api/user/UserFactory.java
+++
@@ -1,8 +0,0 @@
-package io.neskdev.api.user;
-
-import org.bson.Document;
-
-public interface UserFactory {
-    User fromJson(String json);
-    User fromDocument(Document document);
-}
